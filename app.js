c41b8f3b228333e4454161b17c07228b1afcdfc4
Anders <lundback.anders@gmail.com>
2019-08-13 23:29:35 +0100

    added middleware

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+// allow cross origin requests
+app.use(cors());
+
+mongoose.connect('mongodb+srv://anders:colorado@gql-lundback-zbalx.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
